5ed2ba240221b5430f07a7d859bf33e7fd0a94bf
Jens Ottmann <jottmann@nanocosmos.de>
2017-12-18 18:05:13 +0100

    use separate function to convert query result to user list

--- app/daos/UserDao.scala
+++ app/daos/UserDao.scala
@@ -15,7 +15,7 @@ import com.mohiva.play.silhouette.api.LoginInfo
 import daos.schema.{LoginInfoTableDef, ProfileTableDef, SupporterTableDef, UserTableDef}
 import models._
 import models.User._
-import models.database.ProfileDB
+import models.database.{LoginInfoDB, ProfileDB, SupporterDB, UserDB}
 import play.api.db.slick.DatabaseConfigProvider
 import reactivemongo.bson.BSONObjectID
 import slick.driver.JdbcProfile
@@ -140,6 +140,24 @@ class MariadbUserDao extends UserDao{
   val loginInfos = TableQuery[LoginInfoTableDef]
   val supporters = TableQuery[SupporterTableDef]
 
+  def buildUserListFromResult(result: Seq[(UserDB, ProfileDB, SupporterDB, LoginInfoDB)]) : List[User] = {
+    //foldLeft[Return Data Type](Init Value)(parameter) => function
+    val userList = result.seq.foldLeft[List[User]](Nil)((userList, dbEntry) =>{
+      val supporter : Supporter = dbEntry._3.toSupporter
+      val loginInfo : LoginInfo = dbEntry._4.toLoginInfo
+      val profile : Profile = Profile(loginInfo, dbEntry._2.confirmed, dbEntry._2.email, supporter)
+
+      if(userList.length != 0 && userList.last.id == dbEntry._1.id){
+        //tail = use all elements except the head element
+        //reverse.tail.reverse = erease last element from list
+        userList.reverse.tail.reverse ++ List(userList.last.copy(profiles = userList.last.profiles ++ List(profile)))
+      }else{
+        userList ++ List(User(dbEntry._1.publicId, List(profile), Set()))
+      }
+    })
+    userList
+  }
+
   override def find(loginInfo: LoginInfo): Future[Option[User]] = ???
 
   override def find(userId: UUID): Future[Option[User]] = {
@@ -151,21 +169,7 @@ class MariadbUserDao extends UserDao{
         )} yield(user, profile, supporter, loginInfo)
 
     dbConfig.db.run(action.result).map(result => {
-        //foldLeft[Return Data Type](Init Value)(parameter) => function
-        val userList = result.seq.foldLeft[List[User]](Nil)((userList, dbEntry) =>{
-          val supporter : Supporter = dbEntry._3.toSupporter
-          val loginInfo : LoginInfo = dbEntry._4.toLoginInfo
-          val profile : Profile = Profile(loginInfo, dbEntry._2.confirmed, dbEntry._2.email, supporter)
-
-          if(userList.length != 0 && userList.last.id == dbEntry._1.id){
-            //tail = use all elements except the head element
-            //reverse.tail.reverse = erease last element from list
-            userList.reverse.tail.reverse ++ List(userList.last.copy(profiles = userList.last.profiles ++ List(profile)))
-          }else{
-            userList ++ List(User(dbEntry._1.publicId, List(profile), Set()))
-          }
-        })
-        userList.headOption
+        buildUserListFromResult(result).headOption
     })
   }
 
