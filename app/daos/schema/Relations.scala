a7f388746719d0507777f8da04579dfa0ef28386
Jens Ottmann <jottmann@nanocosmos.de>
2018-02-12 13:14:37 +0100

    user_id is long and not uuid

--- app/daos/schema/Relations.scala
+++ app/daos/schema/Relations.scala
@@ -3,6 +3,7 @@ package daos.schema
 import java.util.UUID
 import slick.driver.MySQLDriver.api._
 
+
 class TaskAccessRightTableDef(tag: Tag) extends Table[(Long, Long)](tag, "Task_AccessRight") {
   def taskId = column[Long]("task_id")
   def accessRightId = column[Long]("access_right_id")
@@ -15,8 +16,10 @@ class TaskAccessRightTableDef(tag: Tag) extends Table[(Long, Long)](tag, "Task_A
   def aRK = foreignKey("accessRightId", accessRightId, TableQuery[TaskTableDef])(_.id, onUpdate = ForeignKeyAction.Cascade)
 }
 
-class UserTaskTableDef(tag: Tag) extends Table[(UUID, Long)](tag, "User_Task"){
-  def userId = column[UUID]("user_id")
+
+
+class UserTaskTableDef(tag: Tag) extends Table[(Long, Long)](tag, "User_Task"){
+  def userId = column[Long]("user_id")
   def taskId = column[Long]("task_id")
 
   def * = (userId, taskId)
@@ -24,3 +27,5 @@ class UserTaskTableDef(tag: Tag) extends Table[(UUID, Long)](tag, "User_Task"){
   def pk = primaryKey("primaryKey", (userId, taskId))
   def tK = foreignKey("taskId", taskId, TableQuery[AccessRightTableDef])(_.id, onUpdate = ForeignKeyAction.Cascade)
 }
+
+
