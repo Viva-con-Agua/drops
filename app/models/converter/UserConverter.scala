fb6c3c1c72292c728004f163e424c420327df41a
Jens Ottmann <jottmann@nanocosmos.de>
2018-02-12 13:02:39 +0100

    consider password info

--- app/models/converter/UserConverter.scala
+++ app/models/converter/UserConverter.scala
@@ -1,16 +1,23 @@
 package models.converter
 
 import com.mohiva.play.silhouette.api.LoginInfo
+import com.mohiva.play.silhouette.api.util.PasswordInfo
 import models.{Profile, Supporter, User}
-import models.database.{LoginInfoDB, ProfileDB, SupporterDB, UserDB}
+import models.database._
 
 object UserConverter {
-  def buildUserListFromResult(result: Seq[(UserDB, ProfileDB, SupporterDB, LoginInfoDB)]) : List[User] = {
+  def buildUserListFromResult(result: Seq[(UserDB, ProfileDB, SupporterDB, LoginInfoDB, Option[PasswordInfoDB])]) : List[User] = {
     //foldLeft[Return Data Type](Init Value)(parameter) => function
     val userList = result.seq.foldLeft[List[User]](Nil)((userList, dbEntry) =>{
       val supporter : Supporter = dbEntry._3.toSupporter
       val loginInfo : LoginInfo = dbEntry._4.toLoginInfo
-      val profile : Profile = Profile(loginInfo, dbEntry._2.confirmed, dbEntry._2.email, supporter)
+      val passwordInfo : Option[PasswordInfo] = if(dbEntry._5.isDefined){
+        Option(dbEntry._5.get.toPasswordInfo)
+      }else{
+        None
+      }
+
+      val profile : Profile = Profile(loginInfo, dbEntry._2.confirmed, dbEntry._2.email, supporter, passwordInfo)
 
       if(userList.length != 0 && userList.last.id == dbEntry._1.id){
         //tail = use all elements except the head element
