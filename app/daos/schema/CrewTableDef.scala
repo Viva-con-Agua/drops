adad8b78452eb29a8b5b45ae4008a25125b42cac
Jens Ottmann <jottmann@nanocosmos.de>
2018-02-20 00:58:23 +0100

    bugfix - use correct table name

--- app/daos/schema/CrewTableDef.scala
+++ app/daos/schema/CrewTableDef.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 import models.database.CrewDB
 import slick.driver.MySQLDriver.api._
 
-class CrewTableDef(tag: Tag) extends Table[CrewDB](tag, "City") {
+class CrewTableDef(tag: Tag) extends Table[CrewDB](tag, "Crew") {
 
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def publicId = column[UUID]("publicId")
