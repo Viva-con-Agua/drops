9efcdcf6813f908a3df183f4e1187ac0864df80b
Jens Ottmann <jottmann@nanocosmos.de>
2018-02-20 00:58:01 +0100

    add apply and convert function to handle crew and crewdb objects

--- app/models/database/CrewDB.scala
+++ app/models/database/CrewDB.scala
@@ -2,17 +2,24 @@ package models.database
 
 import java.util.UUID
 
+import models.Crew
+
 case class CrewDB(
                  id: Long,
                  publicId: UUID,
                  name: String,
                  country: String
-               )
+               ) {
+  def toCrew(cities : Set[String]) : Crew = Crew(publicId, name, country, cities)
+
+}
 
 object CrewDB{
   def mapperTo(
               id: Long, publicId: UUID, name: String, country: String
               ) = apply(id, publicId, name, country)
+
+  def apply (crew: Crew): CrewDB = CrewDB(0, crew.id, crew.name, crew.country)
 }
 
 
