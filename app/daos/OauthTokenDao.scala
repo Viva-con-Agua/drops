7ce7a5c2a8fb84de4f84ae38311c4e17156fb903
Jens Ottmann <jottmann@nanocosmos.de>
2018-02-28 14:49:18 +0100

    add OauthToken Support for MariaDB

--- app/daos/OauthTokenDao.scala
+++ app/daos/OauthTokenDao.scala
@@ -2,16 +2,25 @@ package daos
 
 import java.util.UUID
 
+import daos.schema.{OauthClientTableDef, OauthTokenTableDef, UserTableDef}
+
 import scala.concurrent.Future
 import models.{OauthClient, OauthToken}
 import models.OauthToken._
+import models.converter.OauthTokenConverter
+import models.database.OauthTokenDB
+import play.api.Play
 import play.api.Play._
 import play.api.libs.json.Json
 import play.api.Play.current
+import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.modules.reactivemongo.ReactiveMongoApi
 import play.modules.reactivemongo.json._
 import play.modules.reactivemongo.json.collection.JSONCollection
+import slick.driver.JdbcProfile
+import slick.lifted.TableQuery
+import slick.driver.MySQLDriver.api._
 
 import scalaoauth2.provider.AccessToken
 
@@ -78,4 +87,87 @@ class MongoOauthTokenDao extends OauthTokenDao {
       case Some(t) => update(token)
       case None => save(token)
     })
+}
+
+class MariadbOauthTokenDao extends OauthTokenDao {
+  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
+  val oauthTokens = TableQuery[OauthTokenTableDef]
+  val oauthClients = TableQuery[OauthClientTableDef]
+  val users = TableQuery[UserTableDef]
+
+  def find(id: Long) : Future[Option[OauthToken]] = {
+    val action = for{
+      ((oauthToken, oauthClient), user) <- (oauthTokens.filter(_.id === id)
+        join oauthClients on (_.clientId === _.id)
+        join users on (_._1.userId === _.publicId)
+        )} yield (oauthToken, oauthClient, user)
+
+    dbConfig.db.run(action.result).map(r => OauthTokenConverter.buildOauthTokenObjectFromResult(r))
+  }
+
+  override def find(uuid: UUID) : Future[Option[OauthToken]] = {
+    val action = for{
+      ((oauthToken, oauthClient), user) <- (oauthTokens.filter(_.userId === uuid)
+        join oauthClients on (_.clientId === _.id)
+        join users on (_._1.userId === _.publicId)
+      )} yield (oauthToken, oauthClient, user)
+
+    dbConfig.db.run(action.result).map(r => OauthTokenConverter.buildOauthTokenObjectFromResult(r))
+  }
+
+  override def find(uuid: UUID, oauthClient: OauthClient) : Future[Option[OauthToken]] = {
+    //ToDo: is that enough to verfiy the OAuthClient?
+    val action = for{
+      ((oauthToken, oauthClient), user) <- (oauthTokens.filter(_.userId === uuid)
+        join oauthClients.filter(oC => oC.id === oauthClient.id && oC.secret === oauthClient.secret) on (_.clientId === _.id)
+        join users on (_._1.userId === _.publicId)
+        )} yield (oauthToken, oauthClient, user)
+
+    dbConfig.db.run(action.result).map(r => OauthTokenConverter.buildOauthTokenObjectFromResult(r))
+  }
+
+  override def find(accessToken: AccessToken) : Future[Option[OauthToken]] = {
+    //ToDo: is that enough to verfiy the accessToken?
+    val action = for{
+      ((oauthToken, oauthClient), user) <- (oauthTokens.filter(_.token === accessToken.token)
+        join oauthClients on (_.clientId === _.id)
+        join users on (_._1.userId === _.publicId)
+        )} yield (oauthToken, oauthClient, user)
+
+    dbConfig.db.run(action.result).map(r => OauthTokenConverter.buildOauthTokenObjectFromResult(r))
+  }
+
+  override def find(accessToken: String) : Future[Option[OauthToken]] = {
+    val action = for{
+      ((oauthToken, oauthClient), user) <- (oauthTokens.filter(_.token === accessToken)
+        join oauthClients on (_.clientId === _.id)
+        join users on (_._1.userId === _.publicId)
+        )} yield (oauthToken, oauthClient, user)
+
+    dbConfig.db.run(action.result).map(r => OauthTokenConverter.buildOauthTokenObjectFromResult(r))
+  }
+  override def findByRefresh(refreshToken: String) : Future[Option[OauthToken]] = {
+    val action = for {
+      ((oauthToken, oauthClient), user) <- (oauthTokens.filter(_.refreshToken === refreshToken)
+        join oauthClients on (_.clientId === _.id)
+        join users on (_._1.userId === _.publicId)
+        )} yield (oauthToken, oauthClient, user)
+
+    dbConfig.db.run(action.result).map(r => OauthTokenConverter.buildOauthTokenObjectFromResult(r))
+  }
+
+  def findDBModel(accessToken: AccessToken) : Future[Option[OauthTokenDB]] = {
+    dbConfig.db.run(oauthTokens.filter(_.token === accessToken.token).result).map(_.headOption)
+  }
+
+  override def save(token: OauthToken) : Future[OauthToken] = createOrUpdate(token)
+
+  override def update(token: OauthToken) : Future[OauthToken] = createOrUpdate(token)
+
+  override def createOrUpdate(token: OauthToken) : Future[OauthToken] = {
+    findDBModel(token.accessToken).flatMap(t => t.isDefined match {
+      case true => dbConfig.db.run(oauthTokens.filter(_.id === t.get.id).update(token.toOauthTokenDBWithId(t.get.id))).flatMap(_ => find(t.get.id))
+      case false => dbConfig.db.run((oauthTokens returning oauthTokens.map(_.id)) += token.toOauthTokenDB).flatMap(r => {find(r)})
+    }).map(_.get)
+  }
 }
--- app/models/database/OauthTokenDB.scala
+++ app/models/database/OauthTokenDB.scala
@@ -1,5 +1,21 @@
 package models.database
 
-class OauthTokenDB {
+import java.util.{Date, UUID}
 
+case class OauthTokenDB (
+    id: Long,
+    token: String,
+    refreshToken: Option[String],
+    scope: Option[String],
+    lifeSeconds: Option[Long],
+    createdAt: Date,
+    userId: UUID,
+    clientId: String
+  )
+
+object OauthTokenDB{
+  def mapperTo(
+              id: Long, token: String, refreshToken: Option[String], scope: Option[String],
+              lifeSeconds: Option[Long], createdAt: Date, userId: UUID, clientId: String
+              ) = apply(id, token, refreshToken, scope, lifeSeconds, createdAt, userId, clientId)
 }
