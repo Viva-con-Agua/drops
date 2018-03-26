475eb955df671085eff42ae115b07109d3dd113b
Johann Sell <sell@informatik.hu-berlin.de>
2018-03-26 16:48:44 +0200

    #215 add new route for requesting authorization code

--- app/controllers/OAuth2Controller.scala
+++ app/controllers/OAuth2Controller.scala
@@ -48,7 +48,6 @@ class OAuth2Controller @Inject() (
     *
     * @author Johann Sell
     * @param clientId identifies the client
-    * @param clientSecret secures the communication, if this method is configured.
     * @return
     */
   def getCode(clientId : String) = SecuredAction.async { implicit request => {
@@ -62,4 +61,55 @@ class OAuth2Controller @Inject() (
       case _ => Future.successful(BadRequest(Messages("oauth2server.clientId.notFound")))
     })
   }}
+
+  /**
+    * If a valid client was submitted, a new OAuth code will be generated and send to the clients redirect URI.
+    *
+    * Different possibilities to secure webservice communication are supported. First, you can use no security ('none').
+    * Secondly, you can use a secret ('secret') and last the microservices can be identified using Sluice. Method in use
+    * will be defined in your application.conf
+    *
+    * TODO: Use Scope, extend response type
+    *
+    * @author Johann Sell
+    * @param scope defines the set of visible attributes
+    * @param client_id identifies the client
+    * @param response_type explicitly defines, if a code is wanted
+    * @param state can be used for stateful interaction (e.g. redirect infos; see OAuth 2 Spec)
+    * @param redirect_uri redirect_uri again
+    * @return
+    */
+  def getCodeOAuth2Spec(scope: String, client_id : String, response_type : String, state: String, redirect_uri: String) =
+    SecuredAction.async { implicit request =>
+      response_type match {
+        case "code" => oauthClientDao.find(client_id, None, "authorization_code").flatMap(_ match {
+          case Some(client) => client.redirectUri.map(_ == redirect_uri).getOrElse(true) match {
+            case true => oauthCodeDao.save(OauthCode(request.identity, client)).map(
+              code => code.client.redirectUri.map((uri) => {
+                val queryStringCode = "code=" + code.code
+                val queryStringState = "state=" + state
+                Redirect(uri + "?" + queryStringCode + "&" + queryStringState)
+              }).getOrElse(
+                BadRequest(Messages("oauth2server.clientHasNoRedirectURI"))
+              )
+            )
+            case _ => Future.successful(BadRequest(Messages("oauth2server.redirectUriDoesNotMatch")))
+          }
+          case _ => Future.successful(BadRequest(Messages("oauth2server.clientId.notFound")))
+        })
+        case _ => Future.successful(BadRequest(Messages("oauth2server.responseTypeUnsupported", "code")))
+      }
+    }
+
+  /**
+    * Implement this to return a result when the user is not authenticated.
+    *
+    * As defined by RFC 2616, the status code of the response should be 401 Unauthorized.
+    *
+    * @param request The request header.
+    * @return The result to send to the client.
+    */
+  override def onNotAuthenticated(request: RequestHeader): Option[Future[Result]] = {
+    Some(Future.successful(Unauthorized(Json.obj("error" -> "No access"))))
+  }
 }
