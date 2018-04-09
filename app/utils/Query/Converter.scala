e83bcbdb8f378373b5d48f5db605ed025d3294d9
Jens Ottmann <jottmann@nanocosmos.de>
2018-04-09 14:11:05 +0200

    add function to concat sql actions

--- app/utils/Query/Converter.scala
+++ app/utils/Query/Converter.scala
@@ -3,12 +3,18 @@ package utils.Query
 import play.Logger
 import play.api.libs.json.JsObject
 import slick.driver.MySQLDriver.api._
-import slick.jdbc.SQLActionBuilder
+import slick.jdbc.{PositionedParameters, SQLActionBuilder, SetParameter}
 
 object Converter {
   def astToSQL(ast: QueryAST) : SQLActionBuilder = {
     val query = "SELECT * FROM Crews WHERE " + ast.toSqlStatement
 
-    sql"#$query"
+  def concat(a: SQLActionBuilder, b: SQLActionBuilder): SQLActionBuilder = {
+    SQLActionBuilder(a.queryParts ++ b.queryParts, new SetParameter[Unit] {
+      def apply(p: Unit, pp: PositionedParameters): Unit = {
+        a.unitPConv.apply(p, pp)
+        b.unitPConv.apply(p, pp)
+      }
+    })
   }
 }
