47a3b53eb2e755ecd2d15f75673d3a84764746ff
Johann Sell <sell@informatik.hu-berlin.de>
2018-09-27 13:11:27 +0200

    add default order and fix joining bug

--- app/utils/Query/Converter.scala
+++ app/utils/Query/Converter.scala
@@ -5,6 +5,13 @@ import play.api.libs.json.JsObject
 import slick.driver.MySQLDriver.api._
 import slick.jdbc.{PositionedParameters, SQLActionBuilder, SetParameter}
 
+case class ConverterException(msg: String) extends Exception
+
+case class View(name : String, defaultSort : Sort) {
+  def getSelectSQL : SQLActionBuilder = sql"""SELECT * FROM #$name"""
+  def getCountSQL : SQLActionBuilder = sql"""SELECT COUNT(*) AS User_count FROM #$name"""
+}
+
 case class Page(limit: Long, offset: Option[Long] = None) {
   def toSqlStatement : SQLActionBuilder = {
     offset.map((o) => sql""" OFFSET #$o""") match {
@@ -14,18 +21,28 @@ case class Page(limit: Long, offset: Option[Long] = None) {
   }
 }
 
-case class Converter(view: String, ast: Option[QueryAST], page: Option[Page]) {
-  private val SELECT = sql"""SELECT * FROM #$view"""
-  private val COUNT = sql"""SELECT COUNT(*) AS User_count FROM #$view"""
+case class Sort(attributes: List[String], dir: String = "ASC") {
+  def toSqlStatement : SQLActionBuilder = {
+    attributes.headOption.map((attribute) => sql""" ORDER BY #$attribute""").map((sqlHead) =>
+    Converter.concat(attributes.tail.foldLeft[SQLActionBuilder](sqlHead)(
+      (sql, attribute) => Converter.concat(sql, sql""", #$attribute""")
+    ), sql""" #$dir""")).getOrElse(sql"""""")
+  }
+}
+
+case class Converter(view: View, ast: Option[QueryAST], page: Option[Page]) {
+  private val SELECT = view.getSelectSQL
+  private val COUNT = view.getCountSQL
 
   private def generate(function: SQLActionBuilder) : SQLActionBuilder = {
     val filtered = ast.map((f) => Converter.concat(sql""" WHERE """, f.toSqlStatement)) match {
       case Some(f) => Converter.concat(function, f)
       case None => function
     }
+    val sorted = Converter.concat(filtered, view.defaultSort.toSqlStatement)
     page match {
-      case Some(p) => Converter.concat(filtered, p.toSqlStatement)
-      case _ => filtered
+      case Some(p) => Converter.concat(sorted, p.toSqlStatement)
+      case _ => sorted
     }
   }
 
@@ -34,6 +51,12 @@ case class Converter(view: String, ast: Option[QueryAST], page: Option[Page]) {
 }
 
 object Converter {
+
+  def apply(view: String, ast: Option[QueryAST], page: Option[Page]) : Converter = view match {
+    case "Users" => Converter(View(view, Sort(List("User_id"))), ast, page)
+    case "Crews" => Converter(View(view, Sort(List("Crew_id"))), ast, page)
+    case _ => throw ConverterException(s"There exists no SQL View named '$view'")
+  }
 
   /**
     * Use [[Converter]] instance and [[Converter.toStatement]] instead!
