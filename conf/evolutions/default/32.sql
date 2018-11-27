fee4df6250ec206221d27c34f5f55e1416ecadf7
Johann Sell <sell@informatik.hu-berlin.de>
2018-11-27 12:04:20 +0100

    Update conf/evolutions/default/32.sql
    Co-Authored-By: deinelieblings <dennis_kleber@mailbox.org>

--- conf/evolutions/default/32.sql
+++ conf/evolutions/default/32.sql
@@ -17,3 +17,13 @@ CREATE VIEW Crews AS
 
 DROP VIEW Users;
 DROP VIEW Crews;
+CREATE VIEW Crews AS
+  SELECT DISTINCT Crew.id as Crew_id,
+    uuid_of(Crew.publicId) as Crew_publicId,
+    Crew.name as Crew_name,
+    Crew.country as Crew_country,
+    City.id as City_id,
+    City.name as City_name,
+    City.crew_id as City_crew_id
+  from Crew
+    INNER JOIN City ON Crew.id = City.crew_id;
