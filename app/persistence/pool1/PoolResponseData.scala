a8eb71fae6965b8f56cc71437d72dc96a04fe799
Johann Sell <sell@informatik.hu-berlin.de>
2018-12-19 15:13:58 +0100

    rename JSON attributes

--- app/persistence/pool1/PoolResponseData.scala
+++ app/persistence/pool1/PoolResponseData.scala
@@ -4,7 +4,7 @@ import play.api.libs.json.Json
 
 case class PoolMailSwitch(mail_switch: String)
 
-case class PoolResponseData(status: Int, message: String, data: Option[PoolMailSwitch])
+case class PoolResponseData(code: Int, message: String, response: Option[PoolMailSwitch])
 
 object PoolResponseData {
   implicit val poolMailSwitchFormat = Json.format[PoolMailSwitch]
